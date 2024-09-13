a5df028ed2abc64b41856494d457b5a2e59f3db6
Tiuns <shin.952004@gmail.com>
2024-09-13 15:46:23 +0700

    BotCustom.js
    Signed-off-by: Tiuns <shin.952004@gmail.com>

--- BotCustom.js
+++ BotCustom.js
@@ -1,12 +1,1 @@
-// ==UserScript==
-// @name         BOT FOR CUSTOM FOR SHIN
-// @namespace    https://www.wolvesville.com/
-// @version      9.5
-// @description  Hết hạn vào năm 2100
-// @homepage     https://github.com/xTiuns
-// @author       https://www.facebook.com/profile.php?id=100004195688381
-// @match        https://www.wolvesville.com/
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=wolvesville.com
-// @grant        none
-// @license      MIT
-// @namespace    https://github.com/xTiuns
+
