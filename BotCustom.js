39c8c2c22b6c2475f9b72f8648026534cb09fec5
Tiuns <shin.952004@gmail.com>
2024-09-13 15:28:15 +0700

    BotCustom.js

--- BotCustom.js
+++ BotCustom.js
@@ -2,11 +2,11 @@
 // @name         BOT FOR CUSTOM FOR SHIN
 // @namespace    https://www.wolvesville.com/
 // @version      9.5
-// @description  Script is developed to do almost work when u play custom !
+// @description  Hết hạn vào năm 2100
 // @homepage     https://github.com/xTiuns
 // @author       https://www.facebook.com/profile.php?id=100004195688381
 // @match        https://www.wolvesville.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wolvesville.com
 // @grant        none
 // @license      MIT
-// @namespace    https://github.com/truongcongquyinfo
+// @namespace    https://github.com/xTiuns
